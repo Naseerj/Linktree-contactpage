f8443c21e8fcbb8050e172edf77907acb2941f16
unknown <Naseerj>
2022-11-04 11:08:05 +0100

    Feat: Added the text area and checkbox

--- src/Contact.js
+++ src/Contact.js
@@ -5,54 +5,71 @@ const Contact = () => {
   return (
     <div className="contactpage">
       <div className="contactpage">
-       <div className="ccc">
-       <div className="textdiv">
-       <div className="ogadiv">
-       <div className="textt">
-       <h1 className="header1">Contact Me</h1>
-       </div>
-        <div className="textt">
-        <p className="firstp"> Hi there, contact me to ask me about anything you have in mind.</p>
-        </div>
-       </div>
-       </div>
-        <form className="form" action="">
-          <div className="firstdiv">
-            <div className="label1div">
-              <label className="label1" htmlFor="">First name</label>
+          <div className="textdiv">
+            <div className="ogadiv">
+              <div className="textt">
+                <h1 className="header1">Contact Me</h1>
+              </div>
+              <div className="textt">
+                <p className="firstp">
+                  {" "}
+                  Hi there, contact me to ask me about anything you have in
+                  mind.
+                </p>
+              </div>
             </div>
-            <input
-              id="first_name"
-              type="text"
-              placeholder="Enter your first name"
-            />
           </div>
-          <div className="secondiv">
-            <div className="label2div">
-            <label htmlFor="">Last name</label>
+          <form className="form" action="">
+            <div className="firstdiv">
+              <div className="label1div">
+                <label className="label1" htmlFor="">
+                  First name
+                </label>
+              </div>
+              <input
+                id="first_name"
+                type="text"
+                placeholder="Enter your first name"
+              />
             </div>
-            <input 
-              id="last_name"
-              type="text"
-              placeholder="Enter your last name"
-            />
-          </div>
-          <div>
-          <div className="secondiv">
-            <div className="label2div">
-            <label htmlFor="">Email</label>
+            <div className="secondiv">
+              <div className="label2div">
+                <label htmlFor="">Last name</label>
+              </div>
+              <input
+                id="last_name"
+                type="text"
+                placeholder="Enter your last name"
+              />
             </div>
-            <input 
-              id="email"
-              type="text"
-              placeholder="Enter your last name"
-            />
-          </div>
-          <textarea name="" id="" cols="30" rows="10"></textarea>
-          </div>
-           
-        </form>
-       </div>
+            <div>
+              <div className="thirdiv">
+                <div className="label2div">
+                  <label htmlFor="">Email</label>
+                </div>
+                <input
+                  id="email"
+                  type="text"
+                  placeholder="Enter your last name"
+                />
+              </div>
+            </div>
+            <div className="txtadiv">
+              <div className="txtlabeldiv">
+                <label className="txtlabel" htmlFor="">
+                  Message
+                </label>
+              </div>
+              <textarea name="" id="" cols="60" rows="10" ></textarea>
+            </div>
+
+            <div className="yy">
+            <div className="check">
+            <input className="rr" type="checkbox" /> You agree to providing your data to  who may contact you.
+            </div>
+            </div>
+          </form>
+        
       </div>
     </div>
   );
